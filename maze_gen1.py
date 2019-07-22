ac9ddf839278fa7dedd8f72cc408cb0e7ef2bf2c
sadieh0ugh <43001477+sadieh0ugh@users.noreply.github.com>
2019-07-22 15:02:27 +0100

    working random maze generation
    implementation of recursive backtracking

--- maze_gen1.py
+++ maze_gen1.py
@@ -1,11 +1,10 @@
 
-# https://www.youtube.com/watch?v=_S7VqeaTBGQ
-
 import pygame
-#from CONST import *
+import random
+
 
 ScreenWidth = 950
-ScreenHeight = 592
+ScreenHeight = 529
 
 
 #  COLOURS
@@ -27,14 +26,17 @@ clock = pygame.time.Clock()
 CellsPerSide = 525 // 35
 maze = [['lurd' for i in range(CellsPerSide)] for j in range(CellsPerSide)]
 
-CurrentCell = (0, 0)
+current = (0, 0)
 
-UnvisitedCells = [(i, j) for i in range(CellsPerSide) for j in range(CellsPerSide)]
-UnvisitedCells.remove(CurrentCell)
+unvisited = [(i, j) for i in range(CellsPerSide) for j in range(CellsPerSide)]
+unvisited.remove(current)
 
-visited = [CurrentCell]
+visited = [current]
 
-def DrawMaze(maze, surface):
+def DrawMaze(maze, surface, current):
+
+    pygame.draw.rect(surface, white, (current[1] * 35, current[0] * 35, 35, 35))
+
     for i, line in enumerate(maze):
         for j, element in enumerate(line):
 
@@ -49,6 +51,69 @@ def DrawMaze(maze, surface):
             if 'd' in element:
                 pygame.draw.line(surface, purple, (j * 35, i * 35 + 35), (j * 35 + 35, i * 35 + 35), 4)
 
+def NextMove(current, maze, unvisited, visited):
+
+    Neighbours = []
+
+    if current[0] + 1 < len(maze) and (current[0] + 1, current[1]) in unvisited:
+        Neighbours.append((current[0] + 1, current[1]))
+
+    if current[1] + 1 < len(maze) and (current[0], current[1] + 1) in unvisited:
+        Neighbours.append((current[0], current[1] + 1))
+
+    if current[0] - 1 >= 0 and (current[0] - 1, current[1]) in unvisited:
+        Neighbours.append((current[0] - 1, current[1]))
+
+    if current[1] - 1 >= 0 and (current[0], current[1] - 1) in unvisited:
+        Neighbours.append((current[0], current[1] - 1))
+
+
+    if len(Neighbours) > 0:
+
+        Next_Position = random.choice(Neighbours)
+
+        if current[0] == Next_Position[0]:
+            if Next_Position[1] > current[1]:
+                direction = 'r'
+                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('r', '')
+                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('l', '')
+
+            else:
+                direction = 'l'
+                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('l', '')
+                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('r', '')
+
+        else:
+            if Next_Position[0] > current[0]:
+                direction = 'd'
+                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('d', '')
+                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('u', '')
+
+            else:
+                direction = 'u'
+                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('u', '')
+                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('d', '')
+
+        current = Next_Position
+
+        if current not in visited:
+            visited.append(current)
+
+        if current in unvisited:
+            unvisited.remove(current)
+    else:
+        if len(visited) > 1:
+            visited = visited[:-1]
+            current = visited[-1]
+
+        else:
+            current = (0, 0)
+
+    return maze, current, visited, unvisited
+
+
+
+
 
 running = True
 while running:
@@ -62,23 +127,12 @@ while running:
 
     screen.fill(black)
 
-    DrawMaze(maze, screen)
+    DrawMaze(maze, screen, current)
 
+    maze, current, visited, unvisited = NextMove(current, maze, unvisited, visited)
     pygame.display.flip()
 
 pygame.quit()
 quit()
 
 
-'''
-
-import pygame
-from CONST import *
-
-widthCell = 35
-heightCell = 35
-
-betweenCells = 5
-
-grid = []
-'''
