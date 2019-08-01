4e98306622047b5603dbd27684d58fe613ea91f5
sadieh0ugh <43001477+sadieh0ugh@users.noreply.github.com>
2019-08-01 20:16:22 +0100

    WORKING PRINTING PATH WITH PYGAME
    created class PathRect to print each square of path which is "+" in the text version 
    big realisation was when calling PathRect --> using i*16 , j*16

--- BFS.py
+++ BFS.py
@@ -15,12 +15,25 @@ class Wall(object):
         walls.append(self)
         self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
 
+        #print(pos[0],pos[1])
+
+class PathRect(object):
+
+    def __init__(self, pos):
+
+        pathrects.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 10, 10)
+       # print(pos[0],pos[1])
+
+
+
 
 screen = pygame.display.set_mode((800, 800))
 
 pygame.display.set_caption('BFS')
 clock = pygame.time.Clock()
 walls = []
+pathrects = []
 def createMaze():
     maze = []
     maze.append(["#", "#", "#", "#", "#", "O", "#"])
@@ -66,9 +79,11 @@ def createMaze2():
 
 
 def printMaze(maze, path=""):
+    global path_rect
     for x, pos in enumerate(maze[0]):
         if pos == "O":
             start = x
+
 
     i = start
     j = 0
@@ -92,10 +107,16 @@ def printMaze(maze, path=""):
         for i, col in enumerate(row):
             if (j, i) in pos:
 
-                print("+ ", end="")
+                cross = "+ "
+                print(cross, end="")
+
+                PathRect((i*16,j*16))
+
             else:
                 print(col + " ", end="")
         print()
+
+
 
 
 
@@ -129,6 +150,7 @@ def valid(maze, moves):
     return True
 
 
+
 def findEnd(maze, moves):
     for x, pos in enumerate(maze[0]):
         if pos == "O":
@@ -148,54 +170,66 @@ def findEnd(maze, moves):
 
         elif move == "D":
             j += 1
+    #PathRect((i,j))
 
     if maze[j][i] == "X":
+
         print("Found: " + moves)
         printMaze(maze, moves)
+
         return True
 
+
+
     return False
+    return i
 
 
 # MAIN ALGORITHM
 
 
 
-def GameLoop():
-    nums = queue.Queue()
-    nums.put("")
-    add = ""
-    maze = createMaze2()
+nums = queue.Queue()
+nums.put("")
+add = ""
+maze = createMaze2()
 
-    while not findEnd(maze, add):
-        add = nums.get()
-        # print(add)
-        for j in ["L", "R", "U", "D"]:
-            put = add + j
-            if valid(maze, put):
-                nums.put(put)
-
-
-    running = True
-    while running:
-        clock.tick(90)
-
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                running = False
-            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-                running = False
-
-        screen.fill(black)
-        for wall in walls:
-            pygame.draw.rect(screen, purple, wall.rect)
-
-        pygame.draw.rect(screen, green, end_rect)
-        pygame.draw.rect(screen, white, start_rect)
-
-        pygame.display.update()
+while not findEnd(maze, add):
+    add = nums.get() # gets first value in queue
+    # print(add)
+    for j in ["L", "R", "U", "D"]:
+        put = add + j
+        if valid(maze, put):  # path generate is valid
+            nums.put(put)
+            # add to queue
 
 
+running = True
+while running:
+    clock.tick(90)
 
-GameLoop()
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
+
+    screen.fill(black)
+    for wall in walls:
+        pygame.draw.rect(screen, purple, wall.rect)
+
+
+    for rect in pathrects:
+        pygame.draw.rect(screen, selected_purple, rect.rect)
+
+    pygame.draw.rect(screen, green, end_rect)
+
+    pygame.draw.rect(screen, white, start_rect)
+
+
+    pygame.display.update()
+
+
+
+
 
