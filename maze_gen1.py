9b5a85e2e331091c1aed9d5e394512df97b71550
sadieh0ugh <43001477+sadieh0ugh@users.noreply.github.com>
2019-08-15 16:14:32 +0100

    new function for blitting multilines
    much more efficient than printed each line separately

--- maze_gen1.py
+++ maze_gen1.py
@@ -2,10 +2,10 @@ import pygame
 import random
 
 
-ScreenWidth = 1000
-ScreenHeight = 625
+ScreenWidth = 1100
+ScreenHeight = 605
 
-#  COLOURS
+#  Colours
 
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -20,82 +20,110 @@ screen = pygame.display.set_mode((ScreenWidth,ScreenHeight)) # sets dimensions o
 pygame.display.set_caption("NEA Code")
 clock = pygame.time.Clock()
 
-CellSize = 30
-CellQuotient = 30
-CellsPerSide = ScreenHeight // CellQuotient # 25 x 25 maze, uses DIV to ignore width of lines drawn, range of maze is 25 by 25
-# each cell holds letters Left, Up, Right, Down - showing which walls they have
-maze = [['lurd' for i in range(CellsPerSide)] for j in range(CellsPerSide)]
+font50 = pygame.font.Font("VT323-Regular.ttf", 50)
+font35 = pygame.font.Font("VT323-Regular.ttf", 35)
+font20 = pygame.font.Font("VT323-Regular.ttf", 20)
 
-current = (0, 0) # start position coordinates
+def Drawmaze(mazeLURD, surface, current, CellSize):
 
-unvisited = [(i, j) for i in range(CellsPerSide) for j in range(CellsPerSide)]
-# list of unvisited cells (i, j) being coordinates of top left corner of each cell
-# initially all cells in the grid are unvisited except for the starting cell
-unvisited.remove(current)
+    pygame.draw.rect(surface, white, (current[1] * CellSize, current[0] * CellSize, CellSize, CellSize))
+    # says position of rectangle is top left corner of the current cell x given cell size (cell width/height) so the rectangle fills the whole cell
 
-visited = [current] # at the start of execution the only square in the visited list is the starting cell
+    for x, line in enumerate(mazeLURD): # gives each line of the mazeLURD a count value starting at 0
+        for y, element in enumerate(line): # gives each element (cell) of a line a count value
+            # checks through every cell (element) and detects the letters held within the placeholder string in multi-dimensional array
 
-
-def DrawMaze(maze, surface, current):
-
-    pygame.draw.rect(surface, white, (current[1] * CellSize, current[0] * CellSize, CellSize, CellSize))  # drawing white player
-    # says position of rectangle is top left corner of current cell x 24 (cell width/height) so the rectangle fills the whole cell
-
-    for i, line in enumerate(maze): # gives each line of the maze a count value starting at 0
-        for j, element in enumerate(line): # gives each element (cell) of a line a count value
             if 'l' in element:
-                pygame.draw.line(surface, purple, (j * CellSize, i * CellSize), (j * CellSize , i * CellSize + CellSize), 4)
+                # means 'l' is present in the string so the cell still has its left wall so it is drawn
+                pygame.draw.line(surface, purple, (y* CellSize, x* CellSize), (y* CellSize , x* CellSize + CellSize), 4)
+
             if 'u' in element:
-                pygame.draw.line(surface, purple, (j * CellSize, i * CellSize), (j * CellSize + CellSize, i * CellSize), 5)
+                # means 'u' is present in the string so the cell still has its upper wall so it is drawn
+                pygame.draw.line(surface, purple, (y* CellSize, x* CellSize), (y* CellSize + CellSize, x* CellSize), 5)
+
             if 'r' in element:
-                pygame.draw.line(surface, purple, (j * CellSize + CellSize, i * CellSize), (j * CellSize + CellSize, i * CellSize + CellSize), 5)
+                # means 'r' is present in the string so the cell still has its right wall so it is drawn
+                pygame.draw.line(surface, purple, (y* CellSize + CellSize, x* CellSize), (y* CellSize + CellSize, x* CellSize + CellSize), 5)
+
             if 'd' in element:
-                pygame.draw.line(surface, purple, (j * CellSize, i * CellSize + CellSize), (j * CellSize + CellSize, i * CellSize + CellSize), 5)
+                # means 'd' is present in the string so the cell still has its bottom wall so it is drawn
+                pygame.draw.line(surface, purple, (y* CellSize, x* CellSize + CellSize), (y* CellSize + CellSize, x* CellSize + CellSize), 5)
 
+'''Function that finds all the possible movements to neighbours of the current cell being checked.
+   One of these neighbours is selected at random then the mazeLURD is manipulated to draw path to new cell'''
+def NextMove(current, mazeLURD, unvisited, visited):
 
-def NextMove(current, maze, unvisited, visited):
-
+    # array that holds all the possible movements of the current cell
     Neighbours = []
 
-    if current[0] + 1 < len(maze) and (current[0] + 1, current[1]) in unvisited:
+    '''if the next square from the row (x) is not over the width of the mazeLURD
+      and the neighbour cell being checked is contained within the unvisited list
+      then the square next along the line from current cell is a possible neighbour'''
+
+    if current[0] + 1 < len(mazeLURD) and (current[0] + 1, current[1]) in unvisited:
         Neighbours.append((current[0] + 1, current[1]))
 
-    if current[1] + 1 < len(maze) and (current[0], current[1] + 1) in unvisited:
+    '''if the next square from the column (y) is not over the height of the mazeLURD
+      and the neighbour cell being checked is contained within the unvisited list
+      then the square below the current cell is a possible neighbour'''
+
+    if current[1] + 1 < len(mazeLURD) and (current[0], current[1] + 1) in unvisited:
         Neighbours.append((current[0], current[1] + 1))
+
+    '''if the previous square from the row (x) has the x placement coordinate of 0 or more (the left side of mazeLURD and above)
+    # and the neighbour cell being checked is contained within the unvisited list
+    # then the square before the current cell is a possible neighbour'''
 
     if current[0] - 1 >= 0 and (current[0] - 1, current[1]) in unvisited:
         Neighbours.append((current[0] - 1, current[1]))
 
+    '''if the previous square from the column (y) has the y placement coordinate of 0 or more (the top of the mazeLURD of more)
+    # and the neighbour cell being checked is contained within the unvisited list
+    # then the square above the current cell is a possible neighbour'''
+
     if current[1] - 1 >= 0 and (current[0], current[1] - 1) in unvisited:
         Neighbours.append((current[0], current[1] - 1))
-
-
+    
+    
+    # if there is at least one possible movement 
     if len(Neighbours) > 0:
-
+        
+        # choose the next square (x, y) randomly from the list of possible moves
         Next_Position = random.choice(Neighbours)
 
-        if current[0] == Next_Position[0]:
-            if Next_Position[1] > current[1]:
-                direction = 'r'
-                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('r', '')
-                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('l', '')
+        if current[0] == Next_Position[0]: # if x coordinate of current cell and the selected nex cell is the same
+                                           # this means they are in the same row 
+            if Next_Position[1] > current[1]: # if the y coordinate of the selected next cell is greater than the current cell
+                direction = 'r'               # it means the next cell chosen is to the right of the current
+                mazeLURD[current[0]][current[1]] = mazeLURD[current[0]][current[1]].replace('r', '')
+                # for the current cell remove the 'r' from the string so the right wall for this cell isnt drawn
+                mazeLURD[Next_Position[0]][Next_Position[1]] = mazeLURD[Next_Position[0]][Next_Position[1]].replace('l', '')
+                # when the next cell chose is to the right, remove the 'l' from the string so the left wall for this cell isnt drawn
 
-            else:
+            else: # not right
                 direction = 'l'
-                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('l', '')
-                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('r', '')
+                mazeLURD[current[0]][current[1]] = mazeLURD[current[0]][current[1]].replace('l', '')
+                # for the current cell remove the 'l' from the string so the left wall for this cell isnt drawn
+                mazeLURD[Next_Position[0]][Next_Position[1]] = mazeLURD[Next_Position[0]][Next_Position[1]].replace('r', '')
+                # when the next cell chose is to the left, remove the 'r' from the string so the right wall for this cell isnt drawn
 
-        else:
-            if Next_Position[0] > current[0]:
-                direction = 'd'
-                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('d', '')
-                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('u', '')
+        else: # means the x coordinates of the current cell and selected next cell are not the same 
+              # therefore their y coordinates must be the same, so they are in the same column 
+            if Next_Position[0] > current[0]: # if the x coordinate of the the selected next square is greater than the current cell
+                direction = 'd'               # it means the next cell chosen is below the current square
+                mazeLURD[current[0]][current[1]] = mazeLURD[current[0]][current[1]].replace('d', '')
+                # for the current cell remove the 'd' from the string so the bottom wall for this cell isnt drawn
+                mazeLURD[Next_Position[0]][Next_Position[1]] = mazeLURD[Next_Position[0]][Next_Position[1]].replace('u', '')
+                # when the next cell chose is below, remove the 'u' from the string so the upper wall for this cell isnt drawn
 
             else:
                 direction = 'u'
-                maze[current[0]][current[1]] = maze[current[0]][current[1]].replace('u', '')
-                maze[Next_Position[0]][Next_Position[1]] = maze[Next_Position[0]][Next_Position[1]].replace('d', '')
+                mazeLURD[current[0]][current[1]] = mazeLURD[current[0]][current[1]].replace('u', '')
+                # for the current cell remove the 'u' from the string so the upper wall for this cell isnt drawn
+                mazeLURD[Next_Position[0]][Next_Position[1]] = mazeLURD[Next_Position[0]][Next_Position[1]].replace('d', '')
+                # when the next cell chose is above, remove the 'd' from the string so the bottom wall for this cell isnt drawn
 
+        # set the position of the current cell to the next cell that was selected at random
         current = Next_Position
 
         if current not in visited:
@@ -103,23 +131,36 @@ def NextMove(current, maze, unvisited, visited):
 
         if current in unvisited:
             unvisited.remove(current)
+
+    # if list Neighbours contains no items, there are no possible movements
     else:
-        if len(visited) > 1:
-            visited = visited[:-1]
-            current = visited[-1]
+        if len(visited) > 1: # if visited list holds more than just (0,0)
+            visited = visited[:-1]  # remove last element to go back one step as the algorithm has found a dead end
+            current = visited[-1]   # now set current cell to be the new last element
 
         else:
             current = (0, 0)
 
-
-    return maze, current, visited, unvisited
-
-
-def GameLoop(maze,screen,current,visited,unvisited):
-    font50 = pygame.font.Font("VT323-Regular.ttf", 50)
-    font20 = pygame.font.Font("VT323-Regular.ttf", 20)
+    return mazeLURD, current, visited, unvisited
 
 
+def GameLoop():
+    CellSize = 30
+    CellQuotient = 30
+    CellsPerSide = ScreenHeight // CellQuotient  # 25 x 25 mazeLURD, uses DIV to ignore width of lines drawn, range of mazeLURD is 25 by 25
+    # each cell holds letters Left, Up, Right, Down - showing which walls they have
+
+    mazeLURD = [['lurd' for x in range(CellsPerSide)] for y in range(CellsPerSide)]
+
+    current = (0, 0)  # start position count
+
+    unvisited = [(x, y) for x in range(CellsPerSide) for y in range(CellsPerSide)]
+    # list of unvisited cells (i, j) being position count of each cell
+    # initially all cells in the grid are unvisited except for the starting cell
+
+    unvisited.remove(current)
+
+    visited = [current]  # at the start of execution the only square in the visited list is the starting cell
 
     pygame.mouse.set_pos(CellSize//2,CellSize//2)
 
@@ -132,7 +173,7 @@ def GameLoop(maze,screen,current,visited,unvisited):
     TimePassed = 0
     running = True
     while running:
-        clock.tick(30)
+        clock.tick(45)
 
 
         for e in pygame.event.get():
@@ -176,22 +217,45 @@ def GameLoop(maze,screen,current,visited,unvisited):
 
         pygame.draw.rect(screen, green, (570, 570, CellSize, CellSize))
 
-        DrawMaze(maze, screen, current)
+        Drawmaze(mazeLURD, screen, current, CellSize) # when first called, draws a grid
 
 
-        maze, current, visited, unvisited = NextMove(current, maze, unvisited, visited)
+        mazeLURD, current, visited, unvisited = NextMove(current, mazeLURD, unvisited, visited)
         #pygame.display.flip()
-        #print(maze)
+        #print(mazeLURD)
 
         #end_rect = pygame.Rect()
         #pygame.draw.rect(screen, green, end_rect)
 
-        textSurf, textRect = textObj("Press Space to Start the timer", font20, white)
+        textSurf, textRect = textObj("Press Space to Start", font20, white)
         textRect.center = (820, 70)
         screen.blit(textSurf, textRect)
+
+        textIntroduction = "The maze you see being generated on screen is using the Recursive Backtracking algorithm. \n" \
+                           "The fundamental principles of this algorithm is to begin with a grid and 'carve out' a maze by removing walls"
+
+        blitMultiLines(screen, textIntroduction, (605, 150), font35)
         pygame.display.flip()
     pygame.quit()
     quit()
+
+def blitMultiLines(surface, text, pos, font, colour=pygame.Color('white')):
+    words = [word.split(' ') for word in text.splitlines()]
+    space = font.size(' ')[0]
+    maxWidth, maxHeight = surface.get_size()
+    x, y = pos
+    for line in words:
+        for word in line:
+            wordSurf = font.render(word, 0, colour)
+            wordWidth, wordHeight = wordSurf.get_size()
+            if x + wordWidth >= maxWidth:
+                x = pos[0]
+                y += wordHeight
+            surface.blit(wordSurf, (x, y))
+            x += wordWidth + space
+        x = pos[0]
+        y += wordHeight
+
 
 #function that renders surface for text to be displayed on
 # eliminates box around text being created
@@ -205,20 +269,19 @@ def textObj(text, font, col):
 def button(msg, x, y, w, h, ic, ac, action=None):
     mouse = pygame.mouse.get_pos()
     #print(mouse)    if need to see positions easier for placing
-    click = pygame.mouse.get_pressed()
+    click1 = pygame.mouse.get_pressed()
+    click2 = pygame.mouse.get_pressed()
 
     if x + w > mouse[0] > x and y + h > mouse[1] > y:  # if mouse position is found in range inside drawn box
         pygame.draw.rect(screen, ac,(x, y, w, h)) # then redraw box a lighter shade so it appears interactive
 
-        #if click[0] == 1 and action != None:
-         #   if action == "play":
-          #      LevelSelect()
-
-
-
-        if click[0] == 1 and action != None:
+        if click1[0] == 1 and action != None:
             if action == "play":
-                GameLoop(maze, screen, current, visited, unvisited)
+                LevelSelect()
+
+        if click2[0] == 1 and action != None:
+            if action == "hard":
+                GameLoop()
 
             #elif action == "tutorial":
                 #Tutorial()
@@ -262,5 +325,5 @@ def start_screen():
             if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
                 running = False
 
-#start_screen()
-GameLoop(maze, screen, current, visited, unvisited)
+start_screen()
+#GameLoop()
