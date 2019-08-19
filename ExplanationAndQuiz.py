9aebfdaef70b502e9e28cd3e13dd39fab32d5926
sadieh0ugh <43001477+sadieh0ugh@users.noreply.github.com>
2019-08-19 15:10:38 +0100

    file for holding text to be blitted to screen
    decided to make separate file as to not clog the code

---
+++ ExplanationAndQuiz.py
@@ -0,0 +1,35 @@
+textIntroduction = "The maze you see being generated on screen is using a Recursive Backtracking algorithm. \n\n\n" \
+                    "The fundamental principles of this algorithm are to begin with a grid of squares, " \
+                    "and 'carve out' a maze by removing walls. \n\n\n" \
+                   "As you watch the maze auto-generate, I will explain how this algorithm works in a little more detail. \n" \
+                   "\n\nBe sure to pay attention because there will be a short quiz, and in order to play the game you must get all the questions right!..." \
+                   "\n\n\n           Press 'N' to continue"
+
+
+textAlgorithm1 = "1. Firstly, the starting square of the maze (here the top left) is chosen.\n"
+textAlgorithm2 = "2. Then, all neighbours of the square are identified.\n"
+textAlgorithm3 = "3. One of these neighbouring squares is chosen at random and the path marked by removing walls.\n\n\n"
+textAlgorithm4 = "4. This process is repeated until a square with no possible neighbours is found.\n\n"
+textAlgorithm5 = "5. Then the algorithm traces back to the previous square to check for available neighbours.\n\n"
+textalgorithm6 = "6. Finally, the maze is completely formed when all the squares have no valid neighbours.\n\n" \
+                 "Once you have read all the information...\n press 'N' once more to begin the quiz"
+
+question1 = "Question 1:\n" \
+                "In order for the maze to begin to carve out its path there must be a ______?\n" \
+                "     a. blank display\n" \
+                "     b. grid of squares\n" \
+                "     c. a pre-made solution\n\n\n"
+question2 = "Question 2:\n" \
+                "How is the position of the next square of the maze chosen?\n" \
+                "     a. in alphabetical order\n" \
+                "     b. which ever is closest\n" \
+                "     c. at random\n\n\n"
+question3 = "Question 3:\n" \
+                "How does the algorithm know when the maze has fully generated?\n" \
+                "     a. when all squares have no valid neigbours\n" \
+                "     b. after 100 moves\n" \
+                "     c. when it reaches the bottom right square"
+
+
+
+
